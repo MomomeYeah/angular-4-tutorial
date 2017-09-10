337bd26ae6aff892bf413eadcc96b27ee0fa9d68
Rob Gwynn-Jones <gwynnr@gmail.com>
2017-09-10 15:40:31 +0100

    Integrate angular routing, and separate out heroes/details/routing/etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
 
 @Component({
     selector:       'app-root',
     templateUrl:    './app.component.html',
     styleUrls:      ['./app.component.css'],
-    providers:      [HeroService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
-
-    constructor (
-        private heroService: HeroService
-    ) {}
-
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    getHeroes() {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
 }
